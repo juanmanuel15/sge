248647c3252574523e09eed294891f26c0fa9140
Juan1508 <juanmanuelhc15@outlook.com>
2019-04-09 14:12:58 -0500

    Se va a crear la nueva interfaz

--- php/admin/cursos.php
+++ php/admin/cursos.php
@@ -7,7 +7,7 @@
 ?>
 
 <!-- Modal Agregar Curso -->
-<div class="modal fade" id="modalCurso" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<!--div class="modal fade" id="modalCurso" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
@@ -291,7 +291,8 @@
       </div>
     </div>
   </div>
-</div>
+
+<!--/div-->
 
 <!DOCTYPE html>
 <html lang="es">
